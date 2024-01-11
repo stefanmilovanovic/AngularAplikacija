f0d736530d4c9625355ae7258b2ebd9a896694ff
stefanmilovanovic <stefdevelops@gmail.com>
2024-01-11 21:14:35 +0100

    Dodata mini test funkcija ucitavanja liste poruka

--- src/app/posta/posta-lista/posta-lista.component.ts
+++ src/app/posta/posta-lista/posta-lista.component.ts
@@ -7,11 +7,13 @@ import { PostaService, SvaPostaOdgovor } from '../posta.service';
   styleUrls: ['./posta-lista.component.css']
 })
 export class PostaListaComponent implements OnInit{
+  ucitavanje = true;
   constructor(private postaService:PostaService){}
   posta:SvaPostaOdgovor[] = [];
   ngOnInit(): void {
     this.postaService.svaPosta().subscribe((response)=>{
       this.posta = response;
+      this.ucitavanje = false;
     })
   }
 }
